cc907aae66bbee6c4e85c9eef7c580da12872751
Silambazhagii <silambazhagii@gmail.com>
2024-01-11 16:17:48 +0530

    updated

--- React_Form/src/App.jsx
+++ React_Form/src/App.jsx
@@ -49,6 +49,7 @@ function App() {
           <input
             {...register('email', {
               required: 'Email is required',
+              pattern:{value:/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/ ,message:'Invalid email'},
               minLength: { value: 10, message: 'Minimum length is 5 characters' },
               maxLength: { value: 40, message: 'Maximum length is 20 characters' },
             })}
